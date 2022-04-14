77cdf20474a92fbb35255548933d3783214bf514
Stéphane L <98824978+StephaneLab@users.noreply.github.com>
2022-04-14 18:14:32 +0200

    Fusion des 2 dépôts sur le trinomes du 2nd degré
    Fusion des 2 dépôts sur le trinomes du 2nd degré
    Renommage des fonctions
    Mises à jour de README.md

---
+++ README.md
@@ -0,0 +1,36 @@
+# Trinome-second-degre-racines-reelles-ou-complexes
+Ce dépôt propose deux fonctions écrites en Python.
+Il propose : 
+
+* Une fonction qui se nomme my_TrinomeR :
+
+La fonction calcule les racines réelles d'un trinôme du second degré quand il y en a.
+
+Elle demande en arguments les coefficients a, b, et c d'un trinôme du second degré T(x) = ax² + bx + c.
+
+La fonction donne :
+- L'orientation de la parabole : les branches tournées vers les y positifs (Parabole convexe), y tournés vers les y négatifs (parabole concave),
+- Le discriminant du trinôme,
+- Les coordonnées du sommet de la parabole,
+- Le nombre de racines du trinôme,
+- Les valeurs réelles des racines du trinômes,
+- La factorisation du trinôme,
+- Une représentation graphique de la parabole.
+
+
+* Une fonction my_TrinomeRC :
+
+La fonction calcule les racines d'un trinôme du second degré, demande de connaître les nombre complexes pour exploiter tous les résultats.
+
+Elle demande en arguments les coefficients a, b, et c d'un trinôme du second degré T(x) = ax² + bx + c.
+
+La fonction donne :
+- L'orientation de la parabole : les branches tournées vers les y positifs (Parabole convexe), y tournés vers les y négatifs (parabole concave),
+- Le discriminant du trinôme,
+- Les coordonnées du sommet de la parabole,
+- Le nombre de racines du trinôme,
+- Les valeurs réelles ou complexes des racines du trinômes,
+- La factorisation du trinôme,
+- Une représentation graphique de la parabole.
+
+Pour ces deux fonctions, les résultats sont données en écriture flottante "float", le programme ne donne pas de résultats sous forme symbolique (par exemple il n'écrira pas racine(3)/2).
---
+++ my_TrinomeRC.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python3.9
+# Licence MIT (c) 2022 Stéphane Lassalvy
+import matplotlib.pyplot as plt
+import numpy as np
+
+def my_TrinomeRC(a, b, c):
+    # Etude d'un trinome du second degré pour les élèves de terminale connaissant les nombres complexes
+    if a == 0:
+        print("La fonction n'est pas une fonction du second degré : Fin.")
+    elif a > 0 or a < 0:
+        print(f"On considère le trinôme ({a})x^2 + ({b})x + ({c})")
+        print()
+    if a > 0:
+        print("a > 0 : la parabole est convexe, avec les branches tournées vers les y positifs")
+    else :
+        print("a < 0 : la parabole est concave, avec les branches tournées vers les y négatifs")
+    print()
+    # Détermination du signe de a
+    signeANum = np.sign(a)
+    if signeANum == -1 :
+       signeAChar = "-"
+       moinsSigneAChat = "+"
+    else :
+        signeAChar = "+"
+        moinsSigneAChar = "-"
+    # Calcul du discriminant
+    delta = b**2 - 4 * a * c
+    print(f"le discriminant Delta vaut : {delta}")
+    print()
+    # Calcul des coordonnées du sommet de la parabole
+    Sx = -b / 2 / a
+    Sy = -delta / 4 / a
+    print("les coordonnées du sommet de la parabole sont:")
+    print(f"Sx = {Sx}")
+    print(f"Sy = {Sy}")
+    print()
+    print(f"La droite verticale d'équation x = {Sx} est un axe de symétrie de la parabole")
+    print()
+    # Si le discriminant delta est strictement positif
+    if delta > 0:
+        print("Delta est strictement positif:")
+        print("Il y a deux racines réelles distinctes x1 et x2.")
+        x1 = (-b - np.sqrt(delta)) / 2 / a
+        x2 = (-b + np.sqrt(delta)) / 2 / a
+        print("Le trinôme se factorise sous la forme :")
+        print("T(x) = a(x - x1)(x - x2)")
+        print(f"a  = {a}")
+        print(f"x1 = {x1}")
+        print(f"x2 = {x2}")
+        print("La parabole coupe l'axe des abscisses deux points distincts d'abscisses respectives x1 et x2.")
+        print()
+        # Signe du trinôme
+        print("Signe du trinôme : ")
+        print("Le trinôme est du signe de a à l'extérieur des racines,")
+        print("et du signe de -a entre les racines :")
+        print("x    | -Inf  x1    x2  +Inf |")
+        print("-----------------------------")
+        print(f"T(x) |   {signeAChar}    0  {moinsSigneAChar}  0    {signeAChar}  |")
+    # Si delta est nul
+    elif delta == 0:
+        print("Delta est nul :")
+        print("Il y a une racine réelle double x0.")
+        x0 = -b / 2 / a
+        print("Le trinôme se factorise sous la forme :")
+        print("T(x) = a(x - x0)^2")
+        print(f"a  = {a}")
+        print(f"x0 = {x0}")
+        print()
+        print("La parabole coupe l'axe des abscisses en un seul point d'abscisse x0.")
+        print()
+        #Signe du trinôme
+        print("Signe du trinôme : ")
+        print("Le trinôme est toujours du signe de a :")
+        print("x    | -Inf   x0   +Inf |")
+        print("-------------------------")
+        print(f"T(x) |    {signeAChar}    0      {signeAChar} |")
+    # Si delta est strictement négatif
+    elif delta < 0:
+        print("Delta est strictement négatif :")
+        print("Pas de racines réelles et pas de factorisation dans R.")
+        print("Mais il y a deux racines complexes conjuguées z1 et z2 : ")
+        print("z1 = [-b + i * racine(abs(delta)] / 2a")
+        print("z2 = [-b - i * racine(abs(delta)] / 2a")
+        print("T(z) = a(z - z1)(z - z2)")
+        print(f"a  = {a}")
+        #Signe du trinôme
+        print("Signe du trinôme : ")
+        print("Le trinôme est toujours du signe de a :")
+        print("x    | -Inf       +Inf |")
+        print("------------------------")
+        print(f"T(x) |        {signeAChar}        |")
+        print()
+        print()
+        z1 = complex(-b / 2 / a, -np.sqrt(abs(delta)) / 2 / a)
+        z2 = z1.conjugate()
+        print(f"z1 = {z1}")
+        print(f"z2 = {z2}")
+        print("où j est le nombre complexe tel que j² = -1 (habituellement noté i).")
+        print()
+        print("La parabole ne coupe pas l'axe des abscisses.")
+        print()
+    # Courbe représentative de la fonction trinôme
+    lx = np.linspace(Sx - 10,  Sx + 10, 50)
+    ly = a*lx ** 2 + b*lx + c
+    plt.xlabel('Axe des abscisses')  # étiquette sur l'axe horizontal
+    plt.ylabel('Axe des ordonnées')  # étiquette sur l'axe vertical
+    plt.axhline(color = 'k')  # tracé de l'axe Ox
+    plt.axvline(color = 'k')  # tracé de l'axe Oy
+    plt.title("Courbe représentative du Trinôme du second degré")
+    plt.plot(lx, ly)
+    print("Fermez si besoin la fenêtre graphique pour terminer le programme")
+    plt.show()
+    print("Fin")
+# Saisie des coefficients du trinôme
+print("Etude du trinôme du second degré T(x) = ax²+bx+c avec a non nul :")
+a = input("Entrez le coefficient a : ")
+b = input("Entrez le coefficient b : ")
+c = input("Entrez le coefficient c : ")
+# Conversion des réponses alphanumériques en numériques (réels)
+a = float(a)
+b = float(b)
+c = float(c)
+# Exécution de la fonction _my_Trinome_Term
+my_TrinomeRC(a, b, c)
